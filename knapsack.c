172512a07ce8553844d56b7672edc54c89440e62
Abhishekms427 <Abhishekms427@gmail.com>
2024-05-17 16:41:28 +0530

    Using Recursion

---
+++ knapsack.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+int count=0;
+int t[4][7];
+
+int max(int a,int b){
+    return a<b?b:a;
+}
+int kp(int wt[],int pr[],int w,int n){
+    count++;
+    if(w==0||n==0){
+        return 0;
+    }
+    if(t[n][w]!=-1){
+        return t[n][w];
+    }
+        if(wt[n-1]<=w){
+            t[n][w]=max((pr[n-1]+kp(wt,pr,w-wt[n-1],n-1)),kp(wt,pr,w,n-1));
+            return t[n][w];
+           
+        }
+        if(wt[n-1]>w) t[n][w]= kp(wt,pr,w,n-1);
+            return t[n][w];
+            
+    
+}
+
+int main(){
+    int wt[] = {1, 2, 3};
+    int pr[] = {10, 15, 40};
+    int n = 3;
+    int W = 6;
+    for(int i=0;i<4;i++){
+        for(int j=0;j<7;j++){
+            t[i][j]=-1;
+        }
+    }
+    printf("%d\n",kp(wt,pr,W,n));
+    printf("%d",count);
+    return 0;
+}
